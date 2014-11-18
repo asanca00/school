95890684ef9638fbea86de094eef381ea2ba4769
Adrian <sanchezalvarezadrian@gmail.com>
2014-11-18 10:56:17 +0100

    proyecto1 change 1

--- proyect1/main.cpp
+++ proyect1/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
-
+#include <qDebug>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "Proyecto 1";
 
     return a.exec();
 }
